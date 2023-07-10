4963ef09e80b08280a21121c96a87947810ce684
cs862 <59978170+cs862@users.noreply.github.com>
2023-07-10 12:27:49 +0100

    security fix

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -24,20 +24,14 @@ function LoginScreen() {
         <>
           <h1>Unlimited films, TV programmes and more.</h1>
           <h2>Watch anywhere. Cancel at any time.</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </h3>
+          <h3>Ready to watch?</h3>
           <div className="loginScreen__input">
-            <form>
-              <input type="email" placeholder="email address" />
-              <button
-                onClick={() => navigate("/home")}
-                className="loginScreen__getStarted"
-              >
-                GET STARTED
-              </button>
-            </form>
+            <button
+              onClick={() => navigate("/home")}
+              className="loginScreen__getStarted"
+            >
+              GET STARTED
+            </button>
           </div>
         </>
       </div>
